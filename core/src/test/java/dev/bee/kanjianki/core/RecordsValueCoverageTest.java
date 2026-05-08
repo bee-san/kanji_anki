bec78175ce5dd4b17a6d40f12052f24b54b47624
bee <github@skerritt.blog>
2026-05-08 14:22:10 +0100

    Avoid dissimilar assert comparison

--- core/src/test/java/dev/bee/kanjianki/core/RecordsValueCoverageTest.java
+++ core/src/test/java/dev/bee/kanjianki/core/RecordsValueCoverageTest.java
@@ -146,7 +146,9 @@ public class RecordsValueCoverageTest {
 
         InkPoint point = new InkPoint(0.25f, 0.5f, 7L);
         assertEquals(new InkPoint(25f, 100f, 7L), point.scaled(100f, 200f));
-        assertNotEquals(point, "not a point");
+        Object nonPoint = "not a point";
+        boolean equalsNonPoint = point.equals(nonPoint);
+        assertFalse(equalsNonPoint);
         assertNotEquals(point, new InkPoint(0.25f, 0.6f, 7L));
         assertEquals(point.hashCode(), new InkPoint(0.25f, 0.5f, 7L).hashCode());
     }
