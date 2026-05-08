34a5357750bc94d0fe382a0c8cb0650070822e63
bee <github@skerritt.blog>
2026-05-08 11:35:59 +0100

    Add SonarQube Cloud analysis workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     id("com.android.application") version "9.1.0" apply false
+    id("org.sonarqube") version "7.3.0.8198"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "bee-san_kanji_anki")
+        property("sonar.organization", "bee-san")
+    }
 }
