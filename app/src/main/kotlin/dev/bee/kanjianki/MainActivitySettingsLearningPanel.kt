958e3405c840597db6a4fa1180a567cdc3a6b306
bee <github@skerritt.blog>
2026-05-20 16:44:59 +0100

    Move learning steps settings to Kotlin

---
+++ app/src/main/kotlin/dev/bee/kanjianki/MainActivitySettingsLearningPanel.kt
@@ -0,0 +1,45 @@
+package dev.bee.kanjianki
+
+import android.view.View
+import android.widget.Toast
+import dev.bee.kanjianki.core.LearningStepsSettingsPolicy
+import dev.bee.kanjianki.core.RecordsSchedulerModels
+import dev.bee.kanjianki.core.SettingsTextCopy
+
+internal class MainActivitySettingsLearningPanel(private val activity: MainActivitySettings) {
+    fun learningStepsSettingsPanel(): View {
+        return learningStepsSettingsPanelView(activity, learningStepsSettingsPanelModel())
+    }
+
+    fun learningStepsSettingsPanelModel(): SettingsLearningStepsPanelModel {
+        val current = activity.store.learningStepSettings()
+        val defaults = RecordsSchedulerModels.LearningStepSettings.defaults()
+        return SettingsLearningStepsPanelModel(
+            title = SettingsTextCopy.learningStepsTitle(),
+            body = SettingsTextCopy.learningStepsBody(),
+            newCardsLabel = MainActivityBase.LABEL_NEW_CARDS,
+            initialNewStepsText = current.newStepsText(),
+            reviewMissesLabel = SettingsTextCopy.reviewMissesLabel(),
+            initialReviewStepsText = current.reviewStepsText(),
+            defaultNewStepsText = defaults.newStepsText(),
+            defaultReviewStepsText = defaults.reviewStepsText(),
+            ankiDefaultLabel = SettingsTextCopy.ankiDefaultLabel(),
+            sameStepsLabel = SettingsTextCopy.sameLearningStepsLabel(),
+            saveLabel = SettingsTextCopy.saveLearningStepsLabel(),
+            onSave = SettingsLearningStepsSaveAction { newStepsText, reviewStepsText ->
+                saveLearningSteps(newStepsText, reviewStepsText)
+            }
+        )
+    }
+
+    private fun saveLearningSteps(newStepsText: String, reviewStepsText: String) {
+        val request = LearningStepsSettingsPolicy.saveRequest(newStepsText, reviewStepsText)
+        if (!request.valid) {
+            Toast.makeText(activity, request.message, Toast.LENGTH_SHORT).show()
+            return
+        }
+        SettingsWriteActions.saveLearningSteps(request, activity.store::saveLearningStepSettings)
+        Toast.makeText(activity, SettingsTextCopy.learningStepsSavedToast(), Toast.LENGTH_SHORT).show()
+        activity.renderSettings()
+    }
+}
