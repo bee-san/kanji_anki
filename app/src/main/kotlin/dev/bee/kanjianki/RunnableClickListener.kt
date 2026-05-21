5a14a9872853891e3d580a555fc65b6e58257b9b
bee <github@skerritt.blog>
2026-05-21 07:32:51 +0100

    Move runnable click listener to Kotlin

---
+++ app/src/main/kotlin/dev/bee/kanjianki/RunnableClickListener.kt
@@ -0,0 +1,11 @@
+package dev.bee.kanjianki
+
+import android.view.View
+
+internal class RunnableClickListener(
+    private val action: Runnable,
+) : View.OnClickListener {
+    override fun onClick(view: View) {
+        action.run()
+    }
+}
