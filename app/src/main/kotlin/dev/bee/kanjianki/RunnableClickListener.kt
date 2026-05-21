9a649d1fa9c84d4f5f10c5e3c859958c160049a3
bee <github@skerritt.blog>
2026-05-21 07:43:30 +0100

    Preserve runnable click listener parity

--- app/src/main/kotlin/dev/bee/kanjianki/RunnableClickListener.kt
+++ app/src/main/kotlin/dev/bee/kanjianki/RunnableClickListener.kt
@@ -3,9 +3,9 @@ package dev.bee.kanjianki
 import android.view.View
 
 internal class RunnableClickListener(
-    private val action: Runnable,
+    private val action: Runnable?,
 ) : View.OnClickListener {
-    override fun onClick(view: View) {
-        action.run()
+    override fun onClick(view: View?) {
+        action!!.run()
     }
 }
