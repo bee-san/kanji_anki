d40deab9dc2fe2b7f996e86dd2f47811f00364db
bee <github@skerritt.blog>
2026-05-21 14:05:19 +0100

    Split settings learning model

--- app/src/main/kotlin/dev/bee/kanjianki/MainActivitySettingsLearningCompose.kt
+++ app/src/main/kotlin/dev/bee/kanjianki/MainActivitySettingsLearningCompose.kt
@@ -47,30 +47,6 @@ private val LearningStepsWhite = Color(0xFFFFFFFF)
 private val LearningStepsPanelShape = RoundedCornerShape(24.dp)
 private val LearningStepsButtonShape = RoundedCornerShape(12.dp)
 
-object SettingsLearningStepsTestTags {
-    const val NEW_STEPS_INPUT = "settings-learning-new-steps-input"
-    const val REVIEW_STEPS_INPUT = "settings-learning-review-steps-input"
-}
-
-fun interface SettingsLearningStepsSaveAction {
-    fun save(newStepsText: String, reviewStepsText: String)
-}
-
-data class SettingsLearningStepsPanelModel(
-    val title: String,
-    val body: String,
-    val newCardsLabel: String,
-    val initialNewStepsText: String,
-    val reviewMissesLabel: String,
-    val initialReviewStepsText: String,
-    val defaultNewStepsText: String,
-    val defaultReviewStepsText: String,
-    val ankiDefaultLabel: String,
-    val sameStepsLabel: String,
-    val saveLabel: String,
-    val onSave: SettingsLearningStepsSaveAction,
-) : SettingsPanelModel
-
 internal fun learningStepsSettingsPanelView(
     activity: MainActivitySettings,
     model: SettingsLearningStepsPanelModel,
