1667fb5699e6e412cae727b97afeabba1b889c97
bee <github@skerritt.blog>
2026-05-21 06:46:53 +0100

    Move study session router to Kotlin

---
+++ app/src/main/kotlin/dev/bee/kanjianki/MainActivityStudySessionRouter.kt
@@ -0,0 +1,15 @@
+package dev.bee.kanjianki
+
+import dev.bee.kanjianki.core.RecordsSchedulerModels
+import dev.bee.kanjianki.core.StudySessionRoute
+
+internal class MainActivityStudySessionRouter(private val home: MainActivityStudy) {
+    fun renderSession(session: RecordsSchedulerModels.StudySession) {
+        when (StudySessionRoute.destination(session)) {
+            StudySessionRoute.Destination.WRITING -> home.renderWritingSession(session)
+            StudySessionRoute.Destination.SIMILAR_KANJI -> home.renderSimilarKanjiSession(session)
+            StudySessionRoute.Destination.MEANING_KANJI -> home.renderMeaningKanjiSession(session)
+            StudySessionRoute.Destination.FLASHCARD -> home.renderFlashcardSession(session)
+        }
+    }
+}
