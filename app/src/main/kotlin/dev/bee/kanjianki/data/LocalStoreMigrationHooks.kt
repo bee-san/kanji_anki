a4f4710f8a34d0a0860ea30a24454ca570c46acf
bee <github@skerritt.blog>
2026-05-21 09:14:25 +0100

    Move migration hooks to Kotlin

---
+++ app/src/main/kotlin/dev/bee/kanjianki/data/LocalStoreMigrationHooks.kt
@@ -0,0 +1,84 @@
+package dev.bee.kanjianki.data
+
+import android.database.sqlite.SQLiteDatabase
+import dev.bee.kanjianki.core.RecordsSyncModels
+
+internal class LocalStoreMigrationHooks(
+    private val store: LocalStoreBase,
+) {
+    fun createTimelineTables(db: SQLiteDatabase) {
+        store.createTimelineTables(db)
+    }
+
+    fun backfillTimelineEvents(db: SQLiteDatabase) {
+        store.backfillTimelineEvents(db)
+    }
+
+    fun addNullableColumn(db: SQLiteDatabase, table: String, column: String, type: String) {
+        store.addNullableColumn(db, table, column, type)
+    }
+
+    fun backfillKanjiInventory(db: SQLiteDatabase, nowMillis: Long, settings: RecordsSyncModels.Settings) {
+        store.backfillKanjiInventory(db, nowMillis, settings)
+    }
+
+    fun rebuildSimilarKanjiChoiceStates(db: SQLiteDatabase, nowMillis: Long) {
+        store.rebuildSimilarKanjiChoiceStates(db, nowMillis)
+    }
+
+    fun backfillLatestHistoricalSync(db: SQLiteDatabase) {
+        store.backfillLatestHistoricalSync(db)
+    }
+
+    fun rebuildStudyItemsWithAnswerSignatureKey(db: SQLiteDatabase) {
+        store.rebuildStudyItemsWithAnswerSignatureKey(db)
+    }
+
+    fun rebuildStudyItemsForLadderScheduler(db: SQLiteDatabase) {
+        store.rebuildStudyItemsForLadderScheduler(db)
+    }
+
+    fun createStudyTaskLogTable(db: SQLiteDatabase) {
+        store.createStudyTaskLogTable(db)
+    }
+
+    fun createStatsIndexes(db: SQLiteDatabase) {
+        store.createStatsIndexes(db)
+    }
+
+    fun ensureStatsAggregateStorage(db: SQLiteDatabase) {
+        store.ensureStatsAggregateStorage(db)
+    }
+
+    fun repairHistoricalSyncSnapshotsIfPossible(db: SQLiteDatabase) {
+        store.repairHistoricalSyncSnapshotsIfPossible(db)
+    }
+
+    fun createKanjiInventoryTables(db: SQLiteDatabase) {
+        store.createKanjiInventoryTables(db)
+    }
+
+    fun createSimilarKanjiTables(db: SQLiteDatabase) {
+        store.createSimilarKanjiTables(db)
+    }
+
+    fun createSimilarKanjiPracticeTables(db: SQLiteDatabase) {
+        store.createSimilarKanjiPracticeTables(db)
+    }
+
+    fun createHistoricalSyncTables(db: SQLiteDatabase) {
+        store.createHistoricalSyncTables(db)
+    }
+
+    fun createImportAuditTables(db: SQLiteDatabase) {
+        store.createImportAuditTables(db)
+    }
+
+    fun addRichReviewColumns(db: SQLiteDatabase) {
+        store.addRichReviewColumns(db)
+    }
+
+    fun addHistoricalIdentityColumns(db: SQLiteDatabase) {
+        store.addHistoricalIdentityColumns(db)
+    }
+}
