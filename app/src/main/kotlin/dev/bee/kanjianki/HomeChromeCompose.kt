27145895c0d433a8f9574ebb42722060f857d55c
bee <github@skerritt.blog>
2026-05-22 17:58:14 +0100

    Remove stale home chrome imports

--- app/src/main/kotlin/dev/bee/kanjianki/HomeChromeCompose.kt
+++ app/src/main/kotlin/dev/bee/kanjianki/HomeChromeCompose.kt
@@ -2,8 +2,6 @@
 
 package dev.bee.kanjianki
 
-import android.view.View
-import android.view.ViewGroup
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -25,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
