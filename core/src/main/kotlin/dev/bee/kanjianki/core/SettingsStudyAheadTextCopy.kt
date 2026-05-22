6ef39ef34f8bfdf95ecc87fec49a219513b7957a
bee <github@skerritt.blog>
2026-05-22 19:53:47 +0100

    Migrate settings interval copy to Kotlin

---
+++ core/src/main/kotlin/dev/bee/kanjianki/core/SettingsStudyAheadTextCopy.kt
@@ -0,0 +1,58 @@
+package dev.bee.kanjianki.core
+
+import java.util.Locale
+
+object SettingsStudyAheadTextCopy {
+    @JvmStatic
+    fun studyAheadTitle(): String = "Study ahead"
+
+    @JvmStatic
+    fun studyAheadBody(): String {
+        return "Pull cards becoming due within this many minutes into the queue. Set 0 to disable. Learning step delays still apply normally (just like Anki)."
+    }
+
+    @JvmStatic
+    fun saveStudyAheadLabel(): String = "Save study ahead"
+
+    @JvmStatic
+    fun studyAheadSavedToast(): String = "Study ahead saved."
+
+    @JvmStatic
+    fun studyAheadMinutesLabel(): String {
+        return String.format(Locale.ROOT, "Minutes (%s)", studyAheadMinutesRange())
+    }
+
+    @JvmStatic
+    fun studyAheadMinutesRange(): String {
+        return String.format(
+            Locale.ROOT,
+            "%d-%d",
+            SettingsInputRules.DEFAULT_STUDY_AHEAD_MINUTES,
+            SettingsInputRules.MAX_STUDY_AHEAD_MINUTES,
+        )
+    }
+
+    @JvmStatic
+    fun studyAheadWholeNumberErrorText(): String {
+        return String.format(Locale.ROOT, "Use a whole number of minutes (%s).", studyAheadMinutesRange())
+    }
+
+    @JvmStatic
+    fun studyAheadOutOfRangeErrorText(): String {
+        return String.format(
+            Locale.ROOT,
+            "Use %d to disable, or up to %s.",
+            SettingsInputRules.DEFAULT_STUDY_AHEAD_MINUTES,
+            studyAheadMaxDescription(),
+        )
+    }
+
+    @JvmStatic
+    fun studyAheadMaxDescription(): String {
+        val maxMinutes = SettingsInputRules.MAX_STUDY_AHEAD_MINUTES
+        if (maxMinutes % 60 == 0) {
+            return String.format(Locale.ROOT, "%d minutes (%dh)", maxMinutes, maxMinutes / 60)
+        }
+        return String.format(Locale.ROOT, "%d minutes", maxMinutes)
+    }
+}
