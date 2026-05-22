c20c0cd47415ef9066aaefc7b06e122659454ef6
bee <github@skerritt.blog>
2026-05-22 17:37:39 +0100

    Tighten settings test helper type

--- app/src/androidTest/kotlin/dev/bee/kanjianki/MainActivitySettingsInstrumentedTestModels.kt
+++ app/src/androidTest/kotlin/dev/bee/kanjianki/MainActivitySettingsInstrumentedTestModels.kt
@@ -2,7 +2,9 @@
 
 package dev.bee.kanjianki
 
-fun settingsUpdatePanelForTest(activity: Any): SettingsUpdatePanelModel {
+import android.app.Activity
+
+fun settingsUpdatePanelForTest(activity: Activity): SettingsUpdatePanelModel {
     val settingsActivity = activity as MainActivitySettings
     return settingsUpdatePageModel(settingsActivity).panel
 }
