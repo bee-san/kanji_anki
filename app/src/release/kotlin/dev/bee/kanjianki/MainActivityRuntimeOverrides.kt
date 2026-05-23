8664505e944ef7e927230ce2e217900ea3d073ee
bee <github@skerritt.blog>
2026-05-23 08:57:20 +0100

    Scope activity overrides by build variant

---
+++ app/src/release/kotlin/dev/bee/kanjianki/MainActivityRuntimeOverrides.kt
@@ -0,0 +1,33 @@
+package dev.bee.kanjianki
+
+import dev.bee.kanjianki.anki.AnkiDroidGateway
+import dev.bee.kanjianki.anki.CollectionGateway
+import dev.bee.kanjianki.study.WritingRecognizer
+import java.util.concurrent.ExecutorService
+
+object MainActivityRuntimeOverrides {
+    @JvmField
+    val ankiDroidGateway: AnkiDroidGateway? = null
+
+    @JvmField
+    val collectionGateway: CollectionGateway? = null
+
+    @JvmField
+    val writingRecognizer: WritingRecognizer? = null
+
+    @JvmField
+    val writingRecognizerFactory: WritingRecognizerFactory? = null
+
+    @JvmField
+    val installPermission: Boolean? = null
+
+    @JvmField
+    val runtimeNotificationPermission: Boolean? = null
+
+    @JvmField
+    val notificationsAllowed: Boolean? = null
+
+    fun interface WritingRecognizerFactory {
+        fun create(executor: ExecutorService): WritingRecognizer?
+    }
+}
