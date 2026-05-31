00691351d0db357539fd66435ae01a0583d46f1d
autumn skerritt <autumnskerritt@autumns-MacBook-Air.local>
2026-05-31 19:34:12 +0100

    test(domain): convert study ratings test to Kotlin

---
+++ domain/src/test/kotlin/dev/bee/kanjianki/core/StudyRatingsTest.kt
@@ -0,0 +1,21 @@
+package dev.bee.kanjianki.core
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
+class StudyRatingsTest {
+    @Test
+    fun normalizeKeepsKnownRatingsAndDefaultsUnknownRatingsToAgain() {
+        assertEquals("again", StudyRatings.AGAIN)
+        assertEquals("hard", StudyRatings.normalize(StudyRatings.HARD))
+        assertEquals("good", StudyRatings.normalize(StudyRatings.GOOD))
+        assertEquals("easy", StudyRatings.normalize(StudyRatings.EASY))
+        assertEquals("again", StudyRatings.normalize(null))
+        assertEquals("again", StudyRatings.normalize("pass"))
+
+        val constructor = StudyRatings::class.java.getDeclaredConstructor()
+        constructor.isAccessible = true
+        assertNotNull(constructor.newInstance())
+    }
+}
