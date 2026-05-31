3e668f1ab081d9834426dc34cd4e574b756370ad
bee-san <bee-san@users.noreply.github.com>
2026-05-31 15:38:27 +0100

    test(app): convert three more JVM tests to Kotlin

---
+++ app/src/test/kotlin/dev/bee/kanjianki/RunnableClickListenerTest.kt
@@ -0,0 +1,21 @@
+package dev.bee.kanjianki
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import java.util.concurrent.atomic.AtomicBoolean
+
+class RunnableClickListenerTest {
+    @Test
+    fun onClickRunsActionWhenViewIsNull() {
+        val clicked = AtomicBoolean(false)
+
+        RunnableClickListener { clicked.set(true) }.onClick(null)
+
+        assertTrue(clicked.get())
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun nullActionFailsWhenClicked() {
+        RunnableClickListener(null).onClick(null)
+    }
+}
