ede6c336951cbf1c78018114c2cedcb0af1c561d
Autumn (Bee) <github@skerritt.blog>
2026-06-08 22:14:46 +0100

    fix(settings): simplify note type copy (#263)

--- core/src/main/kotlin/dev/bee/kanjianki/core/SettingsNoteTypeTextCopy.kt
+++ core/src/main/kotlin/dev/bee/kanjianki/core/SettingsNoteTypeTextCopy.kt
@@ -17,7 +17,7 @@ object SettingsNoteTypeTextCopy {
 
     @JvmStatic
     fun requiredFieldsBody(): String {
-        return "Map expression, reading, meaning, sentence, frequency, sort."
+        return "Map the Anki fields Kani needs."
     }
 
     @JvmStatic
