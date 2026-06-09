489398acda6f825e81fb3c315ac41362de3d787d
bee-san <github@skerritt.blog>
2026-06-09 22:43:09 +0100

    fix(settings): trim note type copy

--- core/src/test/kotlin/dev/bee/kanjianki/core/SettingsTextCopyTest.kt
+++ core/src/test/kotlin/dev/bee/kanjianki/core/SettingsTextCopyTest.kt
@@ -275,7 +275,7 @@ class SettingsTextCopyTest {
                         "Fonts",
                         "Note type",
                         "Using Kiku",
-                        "Use Kiku, or map your own Anki fields.",
+                        "Use Kiku or map your own Anki fields.",
                         "Fields"
                 ),
                 listOf(
@@ -303,7 +303,7 @@ class SettingsTextCopyTest {
         )
         assertEquals(
                 listOf(
-                        "Tell Kani which Anki fields to read.",
+                        "Tell Kani which fields to read.",
                         "Expression field",
                         "Reading field",
                         "Meaning field",
