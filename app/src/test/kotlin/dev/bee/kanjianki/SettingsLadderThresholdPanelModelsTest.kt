6f25e77781a84dd609094bf9d5b6b392e42f162c
bee-san <github@skerritt.blog>
2026-06-09 09:06:17 +0100

    test(settings): update ladder movement label expectation

--- app/src/test/kotlin/dev/bee/kanjianki/SettingsLadderThresholdPanelModelsTest.kt
+++ app/src/test/kotlin/dev/bee/kanjianki/SettingsLadderThresholdPanelModelsTest.kt
@@ -16,7 +16,7 @@ class SettingsLadderThresholdPanelModelsTest {
         }
 
         assertEquals("Ladder movement", model.title)
-        assertEquals("Review days to move up", model.promotionDaysLabel)
+        assertEquals("Days to move up", model.promotionDaysLabel)
         assertEquals("Misses to move down", model.failStreakLabel)
         assertEquals("1", model.initialFailStreakText)
         assertEquals(RecordsBase.DEFAULT_LADDER_PROMOTION_INTERVAL_DAYS.toString(), model.defaultPromotionDaysText)
